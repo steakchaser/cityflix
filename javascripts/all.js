49f596f4784f6e5017270c842e3a11e73dbc0ca0
TJ Moretto <tj.moretto@musclesound.com>
2015-08-15 12:26:06 -0700

    Automated commit at 2015-08-15 19:26:06 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -40502,7 +40502,7 @@ if (Function.prototype.bind && /^object$|^function$/.test(typeof console) && typ
 
 cityflixApp = angular.module('cityflixApp', []);
 (function() {
-  cityflixApp.controller('MovieListCtrl', function($scope, $filter) {
+  cityflixApp.controller('MovieListCtrl', function($filter, $http, $scope) {
     var columnize;
     columnize = function(input, cols) {
       var arr, colIdx, i;
@@ -40516,42 +40516,20 @@ cityflixApp = angular.module('cityflixApp', []);
       }
       return arr;
     };
-    $scope.movies = [
-      {
-        'name': '180'
-      }, {
-        'name': '24 Hours on Craigslist'
-      }, {
-        'name': '48 Hours'
-      }, {
-        'name': '50 First Dates'
-      }, {
-        'name': 'About a Boy'
-      }, {
-        'name': 'After the Thin Man'
-      }, {
-        'name': 'A Jitney Elopement'
-      }, {
-        'name': 'Alcatraz'
-      }, {
-        'name': 'Alexander\'s Ragtime Band'
-      }, {
-        'name': 'All About Eve'
-      }, {
-        'name': 'American Graffiti'
-      }, {
-        'name': 'American Yearbook'
-      }, {
-        'name': 'A Night Full of Rain'
-      }
-    ];
-    $scope.columns = columnize($scope.movies, 3);
+    $http.get('data/movies.json').success(function(data) {
+      var movies;
+      movies = data.map(function(movie, i) {
+        return movie;
+      });
+      $scope.movies = movies;
+      $scope.columns = columnize($scope.movies || [], 3);
+    });
     return $scope.filter = function(query) {
       var filteredMovies;
       filteredMovies = $filter('filter')($scope.movies, {
-        name: query
+        title: query
       });
-      return $scope.columns = columnize(filteredMovies, 3);
+      $scope.columns = columnize(filteredMovies, 3);
     };
   });
 
